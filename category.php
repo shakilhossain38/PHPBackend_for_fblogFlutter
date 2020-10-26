97026d18a7ea535539d86f54b8d628ecb46d6db4
Shakil Hossain <shakilhossain38@gmail.com>
2020-10-27 01:13:42 +0600

    First Commit

---
+++ category.php
@@ -0,0 +1,25 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$db_name="flutter_blog";
+$conn=mysqli_connect($host,$user, $password,$db_name);
+if(!$conn){
+	echo "Not Connected";
+}
+
+$list=array();
+$query= "SELECT * FROM category";
+$result= mysqli_query($conn, $query);
+while ($row=mysqli_fetch_assoc($result)) {
+	//$result=$row;
+	array_push($list,$row);
+}
+
+
+echo json_encode($list);
+
+
+
+?>
